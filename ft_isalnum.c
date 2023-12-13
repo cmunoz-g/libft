d4ca449aa6f7d543028c7f68ce5ec43d0508323b
Carlos <carlosmg.mkt@gmail.com>
2023-12-13 10:25:53 +0100

    c

---
+++ ft_isalnum.c
@@ -0,0 +1,9 @@
+int ft_isalnum(char c)
+{
+    if (c >= 48 && c <= 57) 
+        return (1);
+    else if ((c >= 65 && c <= 98) || (c >= 97 && c <= 122))
+        return (1);
+    else
+        return (0);
+}
---
+++ ft_memset.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+
+void    *ft_memset(void *str, int i, size_t j)
+{
+    
+}
