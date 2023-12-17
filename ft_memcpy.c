03ad193288c4f385f589f7923c36d0008cd5993a
Carlos <carlosmg.mkt@gmail.com>
2023-12-17 12:25:33 +0100

    c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
-#include <string.h>
-
-void    *memcpy(void *dest, const void *src, size_t n)
+void    *ft_memcpy(void *dest, const void *src, size_t n)
 {
+    unsigned char *destcpy;
+    unsigned const char *srccpy;
 
-}
-
-int main()
-{
-    char dest[50];
-    const char src[4] = "hola";
-    
+    destcpy = dest;
+    srccpy = src;
+    while (n > 0)
+    {
+        *destcpy = *srccpy;
+        n--;
+        destcpy++;
+        srccpy++;
+    }
+    return (dest);
 }
---
+++ ft_memmove.c
@@ -0,0 +1,8 @@
+#include <string.h>
+#include <stdio.h>
+
+void    *ft_memmove(void *dest, const void *src, size_t n)
+{
+
+}
+
