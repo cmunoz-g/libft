da083927c6f33ccb653938ce4454aa7eb3fac033
Carlos <carlosmg.mkt@gmail.com>
2023-12-18 12:19:01 +0100

    c

---
+++ ft_atoi.c
@@ -0,0 +1,22 @@
+int	ft_atoi(const char *nptr)
+{
+	int	neg;
+	int	res;
+
+	neg = 1;
+	res = 0;
+	while (*nptr == 32)
+		nptr++;
+	if (*nptr == 43 || *nptr == 45)
+	{
+		if (*nptr == '-')
+			neg = -1;
+		nptr++;
+	}
+	while (*nptr >= 48 && *nptr <= 57)
+	{
+		res = (res * 10) + (*nptr - '0');
+		nptr++;
+	}
+	return (res * neg);
+}
