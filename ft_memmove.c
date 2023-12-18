d479eff2fa52f42c8efb0488390fb5f3cf71651b
Carlos <carlosmg.mkt@gmail.com>
2023-12-18 10:25:11 +0100

    c

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,8 +1,27 @@
-#include <string.h>
-#include <stdio.h>
-
 void    *ft_memmove(void *dest, const void *src, size_t n)
 {
+    unsigned char *d;
+    const unsigned char *s;
+    size_t i;
 
+    d = dest;
+    s = src;
+    i = 0;
+    if (s < d && d < (s + n))
+    {
+        while (n > 0)
+        {
+            d[n - 1] = s[n - 1];
+            n--;
+        }
+    }
+    else
+    {
+        while (i < n)
+        {
+            d[i] = s[i];
+            i++;
+        }
+    }
+    return (dest);
 }
-
