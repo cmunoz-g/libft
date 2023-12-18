8eabefcbeaf09fa41913c9ac2922a46f5702a8cc
Carlos <carlosmg.mkt@gmail.com>
2023-12-18 12:07:17 +0100

    c

---
+++ ft_strnstr.c
@@ -0,0 +1,28 @@
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	char	*ret;
+
+	i = 0;
+	j = 0;
+	if (*needle == '\0')
+		return ((char *)haystack);
+	while (i < len && haystack[i])
+	{
+		if (haystack[i] == needle[j])
+		{
+			ret = (char *)(haystack) + i;
+			while (haystack[i] == needle[j])
+			{
+				i++;
+				j++;
+			}
+			if (!needle[j])
+				return (ret);
+			j = 0;
+		}
+		i++;
+	}
+	return (0);
+}
