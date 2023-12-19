a14f4ec7f2088f7853c6202a683b4c68b8339288
Carlos <carlosmg.mkt@gmail.com>
2023-12-19 14:44:11 +0100

    c

---
+++ ft_strmapi.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+
+}
+
+char	*function(unsigned int n, char c)
+{
+	
+}
+
+int	main()
+{
+	char (*f)(unsigned int, char);
+	f = function;
+}
