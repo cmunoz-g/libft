104ad868f704986854bb9442af1db190ab53d4c2
Carlos <carlosmg.mkt@gmail.com>
2023-12-27 11:01:02 +0100

    c

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,6 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 
 	if (new && lst)
 	{
-		new->next = NULL;
 		if (!(*lst))
 			*lst = new;
 		else
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -7,13 +7,13 @@ size_t  ft_strlcat(char *dest, const char *src, size_t destsize)
     size_t  destlen;
     size_t  srclen;
 
-    if ((!dest || !src ) && destsize == 0)
+    if (!dest || !src)
         return (0);
     srclen = ft_strlen(src);
     destlen = ft_strlen(dest);
     j = destlen;
     i = 0;
-    if (destsize < destlen)
+    if (destsize <= destlen)
         return (srclen + destsize);
     else
     {
